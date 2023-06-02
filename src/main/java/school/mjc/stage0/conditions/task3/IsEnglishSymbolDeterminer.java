35a86e9d36f00feaf431912cd36aacd68142b8b5
mamula2023 <mamulashvili.2017@gmail.com>
2023-06-02 14:57:12 +0400

    bullshit

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,6 +1,13 @@
 package school.mjc.stage0.conditions.task3;
 
+import javax.annotation.processing.SupportedSourceVersion;
+
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        if(symbol >= 'a' && symbol <='z' || (symbol >='A' && symbol <='Z'))
+            System.out.println("English");
+        else
+            System.out.println("Non English");
+
     }
 }
